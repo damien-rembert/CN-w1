16ec09ba66363901e27e25eb3f598b35e23fd0dd
Damien R <damien.rembert@gmail.com>
2022-01-11 10:32:55 +0000

    adding day two

---
+++ day_2.js
@@ -0,0 +1,8 @@
+
+// charAt ignores spaces
+console.log("All around the world".charAt(0).toUpperCase());
+console.log("All around the world".charAt(1).toUpperCase());
+console.log("All around the world".charAt(2).toUpperCase());
+console.log("All around the world".charAt(3).toUpperCase());
+console.log("All around the world".charAt(4).toUpperCase());
+console.log("All around the world".charAt(5).toUpperCase());
